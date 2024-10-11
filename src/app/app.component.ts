2c104fa001c290cf92564d3c2cf6723ed99148f8
CalinP1 <prostean.vasilecalin@gmail.com>
2024-10-11 20:26:51 +0300

    first important style updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { HotelsMainMenuComponent } from './hotels-main-menu/hotels-main-menu.component';
 import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HotelsMainMenuComponent,
-    RouterLink,
-    RouterLinkActive,
-    HeaderComponent,
-  ],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'hotel_booking';
+  title = 'movies_list';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,5 @@
 import { Routes } from '@angular/router';
-import { HotelsUserAccountComponent } from './hotels-user-account/hotels-user-account.component';
-import { HotelsDisplaySearchComponent } from './hotels-display-search/hotels-display-search.component';
-import { HotelsMainMenuComponent } from './hotels-main-menu/hotels-main-menu.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'profile', component: HotelsUserAccountComponent },
-  { path: 'hotels-list', component: HotelsDisplaySearchComponent },
 ];
