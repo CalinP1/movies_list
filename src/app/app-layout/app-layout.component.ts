3eae452589c48393b2bade49da0b8b0af2a185bc
CalinP1 <prostean.vasilecalin@gmail.com>
2024-10-12 16:22:45 +0300

    router outlet

--- src/app/app-layout/app-layout.component.ts
+++ src/app/app-layout/app-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeaderComponent } from '../header/header.component';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-app-layout',
   standalone: true,
-  imports: [NavbarComponent, HeaderComponent],
+  imports: [NavbarComponent, HeaderComponent, RouterOutlet],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { ProfileComponent } from './profile/profile.component';
 import { TvShowsListComponent } from './tv-shows-list/tv-shows-list.component';
+import { HeroPageComponent } from './hero-page/hero-page.component';
+
 export const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: HeroPageComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'movieList', component: MovieListComponent },
   { path: 'tvShowsList', component: TvShowsListComponent },
   { path: 'profile', component: ProfileComponent },
