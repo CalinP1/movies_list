146ee7a3706eb6febc45e7f9a748cb570137fb11
CalinP1 <prostean.vasilecalin@gmail.com>
2024-11-11 21:12:18 +0200

    environment for hiden api key

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environments';
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
-  private apiUrl = 'https://api.watchmode.com/v1/genres/?apiKey=YOUR_API_KEY';
+  private apikey = environment.apiKey;
+  private apiUrl = `https://api.watchmode.com/v1/genres/?apiKey=${this.apikey}`;
   constructor() {}
 }
